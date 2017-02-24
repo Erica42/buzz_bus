02b1f0b9ef4723dfbb44bd14c813c8c17e36480d
Larry Cherry <larry.cherry.0406@gmail.com>
2017-02-24 12:08:21 -0600

    was able to hit the api

--- station_location/app/models/location.rb
+++ station_location/app/models/location.rb
@@ -1,2 +1,11 @@
+#require 'net/http'
 class Location < ApplicationRecord
+
+
+	#def self.pull_stops
+	#	uri = URI('http://instabus.org/data/stops_1_1.json')
+	#	return Net::HTTP.get(uri)
+	#end
 end
+
+
