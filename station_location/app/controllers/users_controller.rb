0d4c0f8a01f1e9e523ef92e933915f5afdf6a416
= <=>
2017-02-25 12:28:32 -0600

    got new user login working

--- station_location/app/controllers/users_controller.rb
+++ station_location/app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      current_user
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully Logged in"
       redirect_to locations_path
     else
       @errors = @user.errors.full_messages
