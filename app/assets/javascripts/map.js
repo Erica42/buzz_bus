e7c75d8db24f0e11c25f7e598284f120bf38cdc3
James Wilson <jbwilson1982@gmail.com>
2017-02-28 09:17:44 -0600

    testing different images for location

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,11 @@
 function setBlueDot(setMap){
   var map = setMap;
-  var location = new google.maps.Marker({ clickable: false,icon: new google.maps.MarkerImage('https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'),
-      // new google.maps.Size(22,22),
-      // new google.maps.Point(0,18),
-      // new google.maps.Point(11,11)),
-      // shadow: null,
-      // zIndex: 999,
+  var location = new google.maps.Marker({ clickable: false,icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+      new google.maps.Size(22,22),
+      new google.maps.Point(0,18),
+      new google.maps.Point(11,11)),
+      shadow: null,
+      zIndex: 999,
       map: map
   });
    return location;
