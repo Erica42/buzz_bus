20b5ca13dea41f8af8264b24df91b46e2aa1b2f6
Larry <email": null>
2017-02-28 14:22:33 -0600

    rspec test for site login works

--- spec/features/logging_in_feature_spec.rb
+++ spec/features/logging_in_feature_spec.rb
@@ -1,11 +1,16 @@
 require "rails_helper"
 
 feature "Login to site" do
-	scenario "the user visits homepage and clicks signin" do 
+	scenario "the user signs up" do 
 		visit '/'
 		click_link('Sign In')
 		#click_link('Sign up now!')
-		find_button('Sign In').visible?
-		binding.pry
+		click_link('Sign up now!')
+		fill_in('Name', :with => 'Test')
+		fill_in('Email', :with => 'Test@email.com')
+		fill_in('Phone', :with => '+18326138479')
+		fill_in('Password', :with => 'Password')
+		click_button('Sign Up')
+		expect(current_path).to eq "/locations"
 	end
 end
